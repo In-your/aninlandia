81ce2502929ee397523673b85d2a9036c3e84a14
In-your <74668465+In-your@users.noreply.github.com>
2021-05-14 14:36:39 +0300

    Add files via upload

---
+++ ledi/katalogja.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function openNav() {
+    document.getElementsById("mySidenav").style.width = "250px";
+    document.getElementsById("main").style.marginleft = "250px";
+    document.body.style.backgroundColor = "rgbs(0,0,0,0.4)";
+}
+ function closeNav() {
+     document.getElementById("mySidenav").style.width = "0";
+     document.getElementById("main").style.marginLeft = "0";;
+     document.body.style.backgroundColor = "white";
+ }
+ 
+ 
